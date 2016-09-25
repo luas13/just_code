ab134cf19076c76443dfa9fcfdd970f7fc7d7bab
Abhishek Kumar <abhipmc@gmail.com>
2016-09-25 14:31:08 -0700

    Update with comments

--- binary_string_no_1s.cpp
+++ binary_string_no_1s.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+// a is an array where a[i] represents no of binary string which ends in 0 
+// and contains no consecutive 1's
+// b is an array where b[i] represents no of binary string which ends in 1 
+// and contains no consecutive 1's
+// We can append either 0 or 1 to a string ending in 0, but we can only append 0 
+// to a string ending in 1. This yields the recurrence relation:
+
+// a[i] = a[i - 1] + b[i - 1]
+// b[i] = a[i - 1] 
+
+The base cases of above recurrence are a[1] = b[1] = 1. The total number of strings of length i is just a[i] + b[i]. 
+
 int computeBinaryStringWithNo1stogether(int n)
 {
 	int* a = (int*)malloc(sizeof(int) * n);

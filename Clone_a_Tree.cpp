9d4a6427b25687b4816c884f3f9282c64906e5e8
Abhishek Kumar <akuma151@asu.edu>
2018-02-18 22:29:02 -0700

    Create Clone_a_Tree.cpp

---
+++ Clone_a_Tree.cpp
@@ -0,0 +1,18 @@
+typdef struct node{
+  int data;
+  node* left;
+  node* right;
+}mynode;
+
+mynode *clone(root)
+{
+   if (root == NULL)
+      return root;
+ 
+   //create new node and make it a copy of node pointed by root
+   node *temp = (node *)malloc(sizeof(node)) ;
+   temp->data = root-> data;    //copying data
+   temp->left = clone(root -> left);    //cloning left child
+   temp->right = clone(root -> right);  //cloning right child
+   return temp;
+}

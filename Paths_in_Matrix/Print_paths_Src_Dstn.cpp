fbefb500d612d8119357ae31549a2c6be49b0099
Abhishek Kumar <akuma151@asu.edu>
2018-02-18 19:46:30 -0700

    Update Print_paths_Src_Dstn.cpp

--- Paths_in_Matrix/Print_paths_Src_Dstn.cpp
+++ Paths_in_Matrix/Print_paths_Src_Dstn.cpp
@@ -1,46 +1,71 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+# include <vector>
 using namespace std;
 
-/* print all the paths robot can take to reach cell(n-1,n-1)
-   from cell(0), given a maze in the form of a matrix of size nxn
-   and the robot can move only 'Right' and 'Down'
-   (r_idx,c_idx) -> starting point of robot      
-*/
-void printRobotPaths(string path,int row,int col,int r_idx,int c_idx) {
-   if ((r_idx == row-1) && (c_idx == col-1)) {
-      cout<<path<<endl;
-      return;
-   }
-   if (r_idx == row-1) {
-      printRobotPaths(path+'D',row,col,r_idx,c_idx+1);
-      return;
-   }
-   if (c_idx == col-1) {
-      printRobotPaths(path+'R',row,col,r_idx+1,c_idx);
-      return;
-   }
-   printRobotPaths(path+'D',row,col,r_idx,c_idx+1);
-   printRobotPaths(path+'R',row,col,r_idx+1,c_idx);
+/* mat: Pointer to the starting of mXn matrix
+i, j: Current position of the robot (For the first call use 0,0)
+m, n: Dimentions of given the matrix
+pi: Next index to be filed in path array
+*path[0..pi-1]: The path traversed by robot till now (Array to hold the
+				path need to have space for at least m+n elements) */
+void printAllPathsUtil(int *mat, int i, int j, int m, int n, vector<int> &path, int pi)
+{
+	// Reached the bottom of the matrix so we are left with
+	// only option to move right
+	if (i == m - 1)
+	{
+		for (int k = j; k < n; k++)
+			path[pi + k - j] = *((mat + i*n) + k);
+		for (int l = 0; l < pi + n - j; l++)
+			cout << path[l] << " ";
+		cout << endl;
+		return;
+	}
+
+	// Reached the right corner of the matrix we are left with
+	// only the downward movement.
+	if (j == n - 1)
+	{
+		for (int k = i; k < m; k++)
+			path[pi + k - i] = *((mat + k*n) + j);
+		for (int l = 0; l < pi + m - i; l++)
+			cout << path[l] << " ";
+		cout << endl;
+		return;
+	}
+
+	// Add the current cell to the path being generated
+	path[pi] = *((mat + i*n) + j);
+
+	// Print all the paths that are possible after moving down
+	printAllPathsUtil(mat, i+1, j, m, n, path, pi + 1);
+
+	// Print all the paths that are possible after moving right
+	printAllPathsUtil(mat, i, j+1, m, n, path, pi + 1);
+
+	// Print all the paths that are possible after moving diagonal
+	// printAllPathsUtil(mat, i+1, j+1, m, n, path, pi + 1);
 }
 
-// main
-int main() {
-   // dimensions of the maze is row x col
-   int row = 3, col = 3;
-   string path;
-   cout<<"\nAll possible paths :-\n";
-   printRobotPaths(path,row,col,0,0);
-   cout<<endl;
-   return 0;
+// The main function that prints all paths from top left to bottom right
+// in a matrix 'mat' of size mXn
+void printAllPaths(int *mat, int m, int n)
+{
+// 	int *path = new int[m+n];
+	vector<int> path((m+n), 0);
+	printAllPathsUtil(mat, 0, 0, m, n, path, 0);
+}
+
+// Driver program to test abve functions
+int main()
+{
+	int mat[2][3] = { {1, 2, 3}, {4, 5, 6} };
+	printAllPaths(*mat, 2, 3);
+	return 0;
 }
 
 /*
-All possible paths :-
-DDRR
-DRDR
-DRRD
-RDDR
-RDRD
-RRDD
+1 4 5 6 
+1 2 5 6 
+1 2 3 6
 */

5aa9d2251f0bbfc7184fb056ca580fe7a9de8b38
Abhishek Kumar <akuma151@asu.edu>
2018-04-09 20:47:52 -0700

    Update Simple_String.cpp

--- Permutation/Simple_String.cpp
+++ Permutation/Simple_String.cpp
@@ -137,3 +137,39 @@ int main() {
 	return 0;
 }
 
+//-----------------------------------------------------------------
+// Soln: 3 Pure recursive
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* Function to print permutations of string
+This function takes three parameters:
+1. String
+2. Starting index of the string
+3. Ending index of the string. */
+void permute(string a, int l, int r)
+{
+int i;
+if (l == r)
+	cout<<a<<endl;
+else
+{
+	for (i = l; i <= r; i++)
+	{
+		swap(a[l], a[i]);
+		permute(a, l+1, r);
+		swap(a[l], a[i]); //backtrack
+	}
+}
+}
+
+/* Driver program to test above functions */
+int main()
+{
+	string str = "ABC";
+	int n = str.length();
+	permute(str, 0, n-1);
+	return 0;
+}
